90cf94297cbf7ca71285773a1a3290a1a0bb00df
atpradical <ivan.dolgikh1199@gmail.com>
2026-03-10 20:14:20 +0300

    fix: remove spaces

--- apps/minglo-blog/docker-entrypoint.sh
+++ apps/minglo-blog/docker-entrypoint.sh
@@ -1,8 +1,8 @@
- #!/bin/sh
-  set -e
+#!/bin/sh
+set -e
 
-  echo "Running migrations..."
-  pnpm run migrate:minglo-blog:deploy
+echo "Running migrations..."
+pnpm run migrate:minglo-blog:deploy
 
-  echo "Starting application..."
-  exec pnpm run start:minglo-blog:prod
+echo "Starting application..."
+exec pnpm run start:minglo-blog:prod

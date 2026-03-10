e0072a656da89a682d7e84b3e829e58f5261681e
atpradical <ivan.dolgikh1199@gmail.com>
2026-03-10 13:44:06 +0300

    feat: add versioning

--- apps/minglo-blog/src/setup/app.setup.ts
+++ apps/minglo-blog/src/setup/app.setup.ts
@@ -1,10 +1,11 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, VersioningType } from '@nestjs/common';
 import { swaggerSetup } from './swagger.setup';
 import { globalPrefixSetup } from '@app/setup/global-prefix.setup';
 import { pipesSetup } from './pipes.setup';
 
 export function appSetup(app: INestApplication, isSwaggerEnabled: boolean) {
   globalPrefixSetup(app);
+  app.enableVersioning({ type: VersioningType.URI });
   pipesSetup(app);
   swaggerSetup(app, isSwaggerEnabled);
 }

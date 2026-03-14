714c8435b8575769bef6dcc1824e3287c9d0c06f
atpradical <ivan.dolgikh1199@gmail.com>
2026-03-15 00:28:15 +0300

    feat: add tests for registration confirmation flow.

--- apps/minglo-blog/test/e2e-tests/auth.e2e-spec.ts
+++ apps/minglo-blog/test/e2e-tests/auth.e2e-spec.ts
@@ -3,15 +3,18 @@ import { App } from 'supertest/types';
 import { initTestSettings } from '../helpers/init-test-settings';
 import { deleteAllData } from '../helpers/delete-all-data';
 import { AuthTestManager } from '../managers/auth-test.manager';
+import { EmailService } from '@app/notifications';
 
 describe('Auth API (e2e)', () => {
   let app: INestApplication<App>;
   let authManager: AuthTestManager;
+  let emailService: jest.Mocked<EmailService>;
 
   beforeAll(async () => {
     const result = await initTestSettings();
     app = result.app;
     authManager = new AuthTestManager(app);
+    emailService = app.get(EmailService);
   });
 
   afterAll(async () => {
@@ -20,6 +23,7 @@ describe('Auth API (e2e)', () => {
 
   beforeEach(async () => {
     await deleteAllData(app);
+    jest.clearAllMocks();
   });
 
   it('204 — should register a new user', async () => {
@@ -30,5 +34,20 @@ describe('Auth API (e2e)', () => {
     await authManager.register(authManager.validDto());
 
     await authManager.register(authManager.validDto({ login: 'otherUser1' }), HttpStatus.CONFLICT);
+  });
+
+  it('204 — should confirm email after registration', async () => {
+    await authManager.register(authManager.validDto());
+
+    const { code } = emailService.sendConfirmationEmail.mock.calls[0][0];
+
+    await authManager.confirmRegistration({ code });
+  });
+
+  it('400 — should fail confirmation with wrong code', async () => {
+    await authManager.confirmRegistration(
+      { code: 'b489bca8-98f3-453f-95cd-1170a018755b' },
+      HttpStatus.BAD_REQUEST,
+    );
   });
 });

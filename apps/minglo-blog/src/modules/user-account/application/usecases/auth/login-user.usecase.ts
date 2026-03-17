8277d392b76022ab3cbd5b0cf1affb0d5b023ea9
Klopov Vladimir <faliingsky@mail.ru>
2026-03-17 20:04:00 +0400

    feat(auth): check confirm email for login

--- apps/minglo-blog/src/modules/user-account/application/usecases/auth/login-user.usecase.ts
+++ apps/minglo-blog/src/modules/user-account/application/usecases/auth/login-user.usecase.ts
@@ -1,11 +1,11 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { UserRepository } from '../../../infrastructure/user.repository';
 import { LoginUserInputDto } from '../../../api/input-dto/login-user.input.dto';
 import { User } from '../../../../../../prisma/generated/prisma/client';
-import { CryptoService } from '../../services/crypto.service';
 import { DomainException, DomainExceptionCode } from '@app/exceptions';
 import { JwtService } from '../../services/jwt.service';
 import { LoginResult } from '../../../api/types/login-result';
+import { CryptoService } from '../../services';
+import { UserRepository } from '../../../infrastructure';
 
 export class LoginUserCommand {
   constructor(public readonly dto: LoginUserInputDto) {}
@@ -23,6 +23,13 @@ export class LoginUserUseCase implements ICommandHandler<LoginUserCommand, Login
     const { dto } = command;
 
     const user: User = await this.userRepository.getByEmail(dto.email);
+
+    if (!user.emailConfirmed) {
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        message: 'Invalid email or password',
+      });
+    }
 
     const isPasswordValid: boolean = await this.cryptoService.comparePassword({
       password: dto.password,

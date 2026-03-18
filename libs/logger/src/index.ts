628057346cec12834323e6237b5eeadd047ac84d
atpradical <ivan.dolgikh1199@gmail.com>
2026-03-19 01:03:18 +0300

    feat: prepare logger library folder structure and files.

---
+++ libs/logger/src/index.ts
@@ -0,0 +1,5 @@
+export * from './logger.module';
+export * from './logger.service';
+export * from './async-local-storage/async-local-storage.service';
+export * from './middleware/request-context.middleware';
+export * from './logger.config';

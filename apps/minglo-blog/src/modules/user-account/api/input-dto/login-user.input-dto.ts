c89791e1d9b48e689f95f969e98ef44ab727da95
atpradical <ivan.dolgikh1199@gmail.com>
2026-04-02 21:13:59 +0300

    fix login validation issue

--- apps/minglo-blog/src/modules/user-account/api/input-dto/login-user.input-dto.ts
+++ apps/minglo-blog/src/modules/user-account/api/input-dto/login-user.input-dto.ts
@@ -1,12 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
 
 export class LoginUserInputDto {
   @ApiProperty({ example: 'Qwerty123!' })
-  @IsString()
   password: string;
 
   @ApiProperty({ example: 'avocado@mail.com', format: 'email' })
-  @IsString()
   email: string;
 }

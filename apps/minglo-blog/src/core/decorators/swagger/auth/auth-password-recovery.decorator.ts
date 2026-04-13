bfccec8156c36cb9ac316dd7ad684a6fd16313e0
Klopov Vladimir <faliingsky@mail.ru>
2026-04-13 13:18:55 +0400

    fix: fix swagger add ApiNotFoundResponse, new-password.usecase - fix DomainException message: 'New password must not match the old password.'

--- apps/minglo-blog/src/core/decorators/swagger/auth/auth-password-recovery.decorator.ts
+++ apps/minglo-blog/src/core/decorators/swagger/auth/auth-password-recovery.decorator.ts
@@ -2,6 +2,7 @@ import { applyDecorators } from '@nestjs/common';
 import {
   ApiBody,
   ApiNoContentResponse,
+  ApiNotFoundResponse,
   ApiOperation,
   ApiTooManyRequestsResponse,
 } from '@nestjs/swagger';
@@ -19,6 +20,9 @@ export function ApiAuthPasswordRecoveryDecorator() {
     }),
     ApiNoContentResponse({
       description: 'If the email exists, a recovery link has been sent.',
+    }),
+    ApiNotFoundResponse({
+      description: 'If the email not-exists',
     }),
     ApiTooManyRequestsResponse({
       description: 'Too many attempts from the same IP address. Please try again later.',

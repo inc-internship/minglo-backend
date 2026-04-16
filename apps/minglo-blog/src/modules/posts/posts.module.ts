acf69db2358e9b9aa9d394bd1876b6ca296a47bd
atpradical <ivan.dolgikh1199@gmail.com>
2026-04-16 19:52:18 +0300

    feat: - connect media service to post module - update core config

--- apps/minglo-blog/src/modules/posts/posts.module.ts
+++ apps/minglo-blog/src/modules/posts/posts.module.ts
@@ -1,16 +1,33 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { PostsController } from './api/posts.controller';
-import { MEDIA_ACCESS_TOKEN_STRATEGY_INJECT_TOKEN } from '@app/media/constants';
+import { MEDIA_ACCESS_TOKEN_STRATEGY_INJECT_TOKEN, MEDIA_SERVICE } from '@app/media/constants';
 import { JwtService } from '@nestjs/jwt';
 import { CoreConfig } from '../../core/core.config';
-import { UploadPostImagesUseCase } from './application/usecases';
+import { CreatePostUseCase, UploadPostImagesUseCase } from './application/usecases';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    ClientsModule.registerAsync([
+      {
+        name: MEDIA_SERVICE,
+        useFactory: (config: CoreConfig) => ({
+          transport: Transport.TCP,
+          options: {
+            host: config.mediaTcpHost,
+            port: config.mediaTcpPort,
+          },
+        }),
+        inject: [CoreConfig],
+      },
+    ]),
+  ],
   controllers: [PostsController],
   providers: [
     UploadPostImagesUseCase,
+    CreatePostUseCase,
     {
       provide: MEDIA_ACCESS_TOKEN_STRATEGY_INJECT_TOKEN,
       useFactory: (config: CoreConfig): JwtService => {

e963048c35255aa23de2174e091ca1dcc6847769
atpradical <ivan.dolgikh1199@gmail.com>
2026-04-17 15:07:47 +0300

    fix: swagger descriptions

--- apps/minglo-blog/src/core/decorators/swagger/posts/create-post.decorator.ts
+++ apps/minglo-blog/src/core/decorators/swagger/posts/create-post.decorator.ts
@@ -24,6 +24,7 @@ export function ApiCreatePostDecorator() {
     }),
     ApiBadRequestResponse({
       type: ErrorResponseBody,
+      description: 'Validation error',
     }),
     ApiCreatedResponse({
       type: CreatedPostViewDto,

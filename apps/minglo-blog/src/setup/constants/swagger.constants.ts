b6c122166852b9ffe3832eace33e5898234a74e9
atpradical <ivan.dolgikh1199@gmail.com>
2026-05-01 21:38:23 +0300

    refactor: - move pipeSetup func to lib - move swaggerSetup func to lib - remove util lib and move error-formatter func to exception lib - remove duplicate funcs pipeSetup from services - add swagger constants for swagger docs in services

---
+++ apps/minglo-blog/src/setup/constants/swagger.constants.ts
@@ -0,0 +1,5 @@
+export const MINGLO_BLOG_SWAGGER_VERSION = '1.0';
+export const MINGLO_BLOG_SWAGGER_PREFIX = 'api/v1';
+export const MINGLO_BLOG_SWAGGER_TITLE = 'Minglo Blog API';
+export const MINGLO_BLOG_SWAGGER_DESCRIPTION =
+  'Backend service for a social media platform inspired by Instagram.';
--- apps/minglo-media-service/src/setup/index.ts
+++ apps/minglo-media-service/src/setup/index.ts
@@ -1,2 +1,1 @@
-export * from './swagger.setup';
 export * from './app.setup';

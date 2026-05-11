ab0a0baeb315bba4beb0cfe39890fb3df0d8c426
Klopov Vladimir <faliingsky@mail.ru>
2026-05-11 16:54:54 +0400

    feat(profile):new endpoint fill my profile

--- apps/minglo-blog/src/core/decorators/swagger/profile/index.ts
+++ apps/minglo-blog/src/core/decorators/swagger/profile/index.ts
@@ -3,3 +3,4 @@ export * from './create-avatar.decorator';
 export * from './view-my-profile.decorator';
 export * from './update-my-profile.decorator';
 export * from './delete-my-profile.decorator';
+export * from './fill-my-profile.decorator';
--- apps/minglo-blog/src/modules/profile/application/usecases/index.ts
+++ apps/minglo-blog/src/modules/profile/application/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './create-avatar.usecase';
 export * from './upload-avatar-image.usecase';
 export * from './update-profile.usecase';
+export * from './fill-profile.usecase';

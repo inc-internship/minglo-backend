ef7dafe913b9c87e3020fa0f03c4004740acfda4
atpradical <ivan.dolgikh1199@gmail.com>
2026-06-27 01:28:29 +0300

    feat(notifications): - update swagger

--- apps/minglo-blog/src/setup/constants/swagger.constants.ts
+++ apps/minglo-blog/src/setup/constants/swagger.constants.ts
@@ -1,5 +1,22 @@
 export const MINGLO_BLOG_SWAGGER_VERSION = '1.0';
 export const MINGLO_BLOG_SWAGGER_PREFIX = 'api/v1';
 export const MINGLO_BLOG_SWAGGER_TITLE = 'Minglo Blog API';
-export const MINGLO_BLOG_SWAGGER_DESCRIPTION =
-  'Backend service for a social media platform inspired by Instagram.';
+export const MINGLO_BLOG_SWAGGER_DESCRIPTION = `
+Backend service for a social media platform inspired by Instagram.
+---
+## 📡 Real-time Notifications
+\`\`\`json
+- ws:socket.io
+- namespace: /notifications
+- auth: Bearer
+
+- event: NOTIFICATION
+- payload: {
+    id: String
+    type: SUBSCRIPTION_ACTIVATED | PAYMENT_REMINDER | SUBSCRIPTION_EXPIRING_7_DAYS | SUBSCRIPTION_EXPIRING_1_DAY
+    message: String,
+    isRead: Boolean,
+    createdAt: Date
+  }
+\`\`\`
+`.trim();

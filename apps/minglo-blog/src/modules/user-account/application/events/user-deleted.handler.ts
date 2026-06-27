538729a87fa1b89a9fcfbb09e0892c7d51de1c62
atpradical <ivan.dolgikh1199@gmail.com>
2026-06-27 22:11:53 +0300

    fix imports

--- apps/minglo-blog/src/modules/user-account/application/events/user-deleted.handler.ts
+++ apps/minglo-blog/src/modules/user-account/application/events/user-deleted.handler.ts
@@ -1,8 +1,8 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { LoggerService } from 'libs/logger/src';
-import { PAYMENT_SERVICE, PAYMENTS_TCP_PATTERNS } from 'libs/payments/src';
+import { LoggerService } from '@app/logger';
+import { PAYMENT_SERVICE, PAYMENTS_TCP_PATTERNS } from '@app/payments';
 
 export class UserDeletedEvent {
   constructor(public readonly userId: string) {}
